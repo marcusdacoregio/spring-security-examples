1c20ddb20cc5d3d34e0b988a4b458846e10fbae4
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-08-29 11:44:52 -0300

    First commit

---
+++ src/test/java/com/example/springsecurityexamples/SpringSecurityExamplesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityexamples;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
