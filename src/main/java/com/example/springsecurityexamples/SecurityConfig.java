bfea64f3a873a349cfc9326cde67265a15b46e9d
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-10-26 15:29:58 -0300

    Sample

--- src/main/java/com/example/springsecurityexamples/SecurityConfig.java
+++ src/main/java/com/example/springsecurityexamples/SecurityConfig.java
@@ -1,14 +1,28 @@
 package com.example.springsecurityexamples;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean
@@ -18,8 +32,26 @@ public class SecurityConfig {
 						.anyRequest().authenticated()
 				)
 				.httpBasic(Customizer.withDefaults())
-				.formLogin(Customizer.withDefaults());
+				.formLogin(Customizer.withDefaults())
+				.exceptionHandling((exceptions) -> exceptions
+						.accessDeniedHandler(new MyHandler())
+				);
 		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	static class MyHandler implements AccessDeniedHandler {
+
+		@Override
+		public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+			response.sendError(418, "Access denied");
+		}
+
 	}
 
 }
--- src/main/java/com/example/springsecurityexamples/SpringSecurityExamplesApplication.java
+++ src/main/java/com/example/springsecurityexamples/SpringSecurityExamplesApplication.java
@@ -2,12 +2,26 @@ package com.example.springsecurityexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityExamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityExamplesApplication.class, args);
+	}
+
+	@RestController
+	static class Controller {
+
+		@GetMapping("/")
+		@PreAuthorize("hasRole('ADMIN')")
+		String hello() {
+			return "hello";
+		}
+
 	}
 
 }
