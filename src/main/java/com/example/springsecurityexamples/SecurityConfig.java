fa19535a7af89c0b8689633c5fed13bad5be7575
Marcus Da Coregio <marcusdacoregio@gmail.com>
2023-10-26 15:32:28 -0300

    Include UserDetailsService and EnableMethodSecurity

--- src/main/java/com/example/springsecurityexamples/SecurityConfig.java
+++ src/main/java/com/example/springsecurityexamples/SecurityConfig.java
@@ -3,12 +3,18 @@ package com.example.springsecurityexamples;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean
@@ -20,6 +26,12 @@ public class SecurityConfig {
 				.httpBasic(Customizer.withDefaults())
 				.formLogin(Customizer.withDefaults());
 		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
