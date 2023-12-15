7e3354900ac6ad0de3b24603f61f1ff59647cc92
Rob Winch <rwinch@users.noreply.github.com>
2023-12-15 15:23:25 -0600

    minimal uses WebHandler directly (cannot reproduce)

--- src/test/java/com/example/springsecurityexamples/MinimalTest.java
+++ src/test/java/com/example/springsecurityexamples/MinimalTest.java
@@ -3,6 +3,12 @@ package com.example.springsecurityexamples;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.server.MatcherSecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterChainProxy;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.server.WebHandler;
+import org.springframework.web.server.handler.DefaultWebFilterChain;
 import reactor.core.publisher.Mono;
 
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +28,12 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class)
 @SecurityTestExecutionListeners
 public class MinimalTest {
@@ -29,49 +41,22 @@ public class MinimalTest {
 	@Test
 	@WithMockUser
 	void minimal() {
-		var filter = new ReactorContextWebFilter(new WebSessionServerSecurityContextRepository());
+		List<WebFilter> sortedWebFilters = new ArrayList<>();
+		sortedWebFilters.add(new ReactorContextWebFilter(new WebSessionServerSecurityContextRepository()));
+		sortedWebFilters.add(new AuthorizationWebFilter(AuthenticatedReactiveAuthorizationManager.authenticated()));
+		var springSecurityFilterChain = new WebFilterChainProxy(new MatcherSecurityWebFilterChain(ServerWebExchangeMatchers.anyExchange(), sortedWebFilters));
+		WebHandler handler = (e) -> {
+			e.getResponse().setStatusCode(HttpStatus.OK);
+			return Mono.empty();
+		};
+		var wfc = new DefaultWebFilterChain(handler, Arrays.asList(springSecurityFilterChain));
 		var request = MockServerHttpRequest.get("/").build();
-		MockServerWebExchange exchange = MockServerWebExchange.builder(request).build();
-		var authorizationManager = AuthenticatedReactiveAuthorizationManager.authenticated();
-		var authorizationFilter = new AuthorizationWebFilter(authorizationManager);
-		MockWebFilter mockWebFilter = new MockWebFilter(filter);
+		MockServerWebExchange exchange = MockServerWebExchange.builder(request)
+				.build();
 
-		mockWebFilter.filter(exchange, ex -> {
-			return authorizationFilter.filter(exchange, (exc) -> Mono.empty());
-		}).block();
+		wfc.filter(exchange).block();
+
+		assertThat(exchange.getResponse().getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
-
-	static class MockWebFilter implements WebFilter {
-
-		private final WebFilter delegate;
-
-		MockWebFilter(WebFilter delegate) {
-			this.delegate = delegate;
-		}
-
-		@Override
-		public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-			return this.delegate.filter(exchange, chain);
-		}
-	}
-
-
-//	@Test
-//	@WithMockUser
-//	void minimal() {
-//		var filter = new ReactorContextWebFilter(new WebSessionServerSecurityContextRepository());
-//		var request = MockServerHttpRequest.get("/").build();
-//		MockServerWebExchange exchange = MockServerWebExchange.builder(request).build();
-//		var authorizationManager = AuthenticatedReactiveAuthorizationManager.authenticated();
-//		var authorizationFilter = new AuthorizationWebFilter(authorizationManager);
-//		filter.filter(exchange, ex -> {
-//			return ReactiveSecurityContextHolder.getContext()
-//					.flatMap((context) -> {
-//						Assertions.assertThat(context.getAuthentication()).isNotNull();
-//						return Mono.empty();
-//					});
-//		}).block();
-//	}
-
 
 }
