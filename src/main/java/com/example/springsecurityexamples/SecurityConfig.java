b752b969b473bb2abfdc8c6df59229d693a565eb
Rob Winch <rwinch@users.noreply.github.com>
2023-12-15 14:17:07 -0600

    Explicit AuthorizationWebFilter

--- src/main/java/com/example/springsecurityexamples/SecurityConfig.java
+++ src/main/java/com/example/springsecurityexamples/SecurityConfig.java
@@ -1,18 +1,17 @@
 package com.example.springsecurityexamples;
 
-import reactor.core.publisher.Mono;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authorization.AuthenticatedReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationWebFilter;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,11 +20,12 @@ public class SecurityConfig {
 	@Bean
 	SecurityWebFilterChain appSecurity(ServerHttpSecurity http) {
 		http
+				.headers(h -> h.disable())
+				.requestCache(r -> r.disable())
+				.logout(l -> l.disable())
 				.csrf((csrf) -> csrf.disable()) // commenting this line makes the test pass
-				.authorizeExchange((authorize) -> authorize
-						.anyExchange().authenticated()
-				)
-				.addFilterAt(new TestWebFilter(), SecurityWebFiltersOrder.CSRF);
+				.addFilterAt(new AuthorizationWebFilter(AuthenticatedReactiveAuthorizationManager.authenticated()), SecurityWebFiltersOrder.AUTHORIZATION);
+//				.addFilterAt(new TestWebFilter(), SecurityWebFiltersOrder.CSRF);
 //				.addFilterAt(new CsrfWebFilter(), SecurityWebFiltersOrder.CSRF); // uncommenting this line makes the test pass
 		return http.build();
 	}
