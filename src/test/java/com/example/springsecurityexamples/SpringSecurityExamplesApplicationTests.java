7fba58300f31e8764187c9b1534e5cc9c851ea39
Rob Winch <rwinch@users.noreply.github.com>
2023-12-15 13:49:01 -0600

    messing around

--- src/test/java/com/example/springsecurityexamples/SpringSecurityExamplesApplicationTests.java
+++ src/test/java/com/example/springsecurityexamples/SpringSecurityExamplesApplicationTests.java
@@ -3,6 +3,11 @@ package com.example.springsecurityexamples;
 import java.time.Duration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.server.MockServerWebExchange;
+import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 import reactor.util.context.ReactorContextAccessor;
 
@@ -16,15 +21,26 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringSecurityExamplesApplicationTests {
 
 	@Autowired
 	WebTestClient client;
 
+	@Autowired
+	WebFilter springSecurityFilterChain;
+
 	@Test
 	@WithMockUser
 	void withMockUserShouldWork() {
+//		var request = MockServerHttpRequest.get("/").build();
+//		MockServerWebExchange exchange = MockServerWebExchange.builder(request).build();
+//		this.springSecurityFilterChain.filter(exchange, (e) -> Mono.empty()).block();
+//
+//		assertThat(exchange.getResponse().getStatusCode()).isEqualTo(HttpStatus.OK);
+
 		this.client.get().uri("/")
 //				.headers((headers) -> headers.setBasicAuth("user", "password"))
 				.exchange()
